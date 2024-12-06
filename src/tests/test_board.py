fb7d83bf9040c799470fd1ec544aedc6b19d0903
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-06 00:02:27 -0600

    Add test for testing board-to-2d representation. Found that this test fails, previously I did not correctly implement converting a board with previous moves to a real bitboard - Currently support only holds for 0 ply boards, meaning it can't keep boards that are of the previous move

--- src/tests/test_board.py
+++ src/tests/test_board.py
@@ -6,22 +6,40 @@ from from_colab.utilities import *
 
 
 # Tests that the 2d input conversion board method will convert the position to the board
-def test_board_to_2d_input():
+def test_board_to_2d_input_0_previous_positions():
     board = chess.Board()
     input_board = board_to_input_2d(board, 0)
+    reconverted_board = input_to_board_2d(input_board, 0)
+
     print(board)
     print(input_board)
-    reconverted_board = input_to_board_2d(input_board, 0)
     print(reconverted_board)
     assert reconverted_board == board  
 
+# Tests that the 2d input conversion board method will convert the position in cases where depth is kept
+def test_board_to_2d_input_5_previous_positions():
+    PLY = 5
+    board = chess.Board()
+    moves = ['e4', 'e5', 'Qh5', 'Nc6', 'Bc4', 'Nf6', 'Qxf7']
+    for moven in moves:
+        board.push_san(moven)
+
+    input_board = board_to_input_2d(board, PLY)
+    reconverted_board = input_to_board_2d(input_board, PLY)
+
+    assert reconverted_board.ply() == PLY
+    assert board.ply() >= PLY
+    assert reconverted_board == board
+    
+    
 def test_move_to_flat():
     board = chess.Board()
     from_square, to_square = ('e2', 'e4')
     move = chess.Move.from_uci(from_square + to_square)
-    print(move)
     flat = move_to_flat(move)
-    print(flat)
     unflat_move = flat_to_move(flat, board)
+    
+    print(move)
+    print(flat)
     print(unflat_move)
     assert unflat_move == move
