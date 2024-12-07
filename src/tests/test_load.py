deb3df202904670f8c9d8f65139150912f465973
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-06 23:11:15 -0600

    Update test_load to test games with a sample test board

--- src/tests/test_load.py
+++ src/tests/test_load.py
@@ -1,4 +1,4 @@
 from from_colab.utilities import load_games_from_pgn
 
 def test_load_games_from_pgn():
-    load_games_from_pgn("bubbakill7_lichess_games.pgn")
+    load_games_from_pgn("tests/test_games.pgn")
