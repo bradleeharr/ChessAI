05bdd314b7d8de91aae4532ce3d06d6e5f8a44ca
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-08 15:28:15 -0600

    Update bitboard to work with multiple layers -- tests appear to somewhat work. Current error in handling ply with multiple boards. TODO fix

--- src/board_representations/bitboard.py
+++ src/board_representations/bitboard.py
@@ -7,16 +7,17 @@ class BitBoard(BoardRepresentation):
 
     def board_to_model_input(self, board):
         pieces_order = [chess.PAWN, chess.ROOK, chess.KNIGHT, chess.BISHOP, chess.QUEEN, chess.KING]
-        board_input = np.zeros((12 * (self.num_previous_positions + 1), 8, 8), dtype=np.float32)
+        board_input = np.zeros(
+            (self.num_previous_positions+1, 12, 8, 8),
+            dtype=np.float32
+        )
 
-        # Fill initial position
         for i, piece in enumerate(pieces_order):
             for square in board.pieces(piece, chess.WHITE):
-                board_input[i, chess.square_rank(square), chess.square_file(square)] = 1
+                board_input[0, i, chess.square_rank(square), chess.square_file(square)] = 1
             for square in board.pieces(piece, chess.BLACK):
-                board_input[i + 6, chess.square_rank(square), chess.square_file(square)] = 1
+                board_input[0, i + 6, chess.square_rank(square), chess.square_file(square)] = 1
 
-        # Fill previous positions
         for k in range(self.num_previous_positions):
             if len(board.move_stack) > 0:
                 last_move = board.peek()
@@ -26,25 +27,27 @@ class BitBoard(BoardRepresentation):
 
             for i, piece in enumerate(pieces_order):
                 for square in board.pieces(piece, chess.WHITE):
-                    board_input[12 * (k + 1) + i, chess.square_rank(square), chess.square_file(square)] = 1
+                    board_input[k, i, chess.square_rank(square), chess.square_file(square)] = 1
                 for square in board.pieces(piece, chess.BLACK):
-                    board_input[12 * (k + 1) + i + 6, chess.square_rank(square), chess.square_file(square)] = 1
+                    board_input[k, i + 6, chess.square_rank(square), chess.square_file(square)] = 1
         return board_input
 
 
-    def model_input_to_board(self, board_input):
+    def model_input_to_board(self, model_input):
         pieces_order = [chess.PAWN, chess.ROOK, chess.KNIGHT, chess.BISHOP, chess.QUEEN, chess.KING]
         board = chess.Board()
         board.clear_board()
 
-        for i, piece in enumerate(pieces_order):
-            for rank in range(8):
-                for file in range(8):
-                    if board_input[i, rank, file] == 1:
-                        square = chess.square(file, rank)
-                        board.set_piece_at(square, chess.Piece(piece, chess.WHITE))
-                    if board_input[i + 6, rank, file] == 1:
-                        square = chess.square(file, rank)
-                        board.set_piece_at(square, chess.Piece(piece, chess.BLACK))
+        for k in range(self.num_previous_positions+1):
+            for i, piece in enumerate(pieces_order):
+                for rank in range(8):
+                    for file in range(8):
+                        if model_input[k, i, rank, file] == 1:
+                            square = chess.square(file, rank)
+                            board.set_piece_at(square, chess.Piece(piece, chess.WHITE))
+                        if model_input[k, i + 6, rank, file] == 1:
+                            square = chess.square(file, rank)
+                            board.set_piece_at(square, chess.Piece(piece, chess.BLACK))
+        
 
         return board
--- tests/test_board_representation.py
+++ tests/test_board_representation.py
@@ -5,7 +5,7 @@ from src.board_representations.board_representation import BoardRepresentation
 from src.board_representations.bitboard import BitBoard 
 from src.board_representations.piecemap import PieceMap
 
-def test_BitBoard_init():
+def test_BoardRepresentation_init():
     prev_pos1 = 0
     prev_pos2 = 5
 
@@ -16,24 +16,28 @@ def test_BitBoard_init():
     assert board2.num_previous_positions == prev_pos2
 
 
-# Tests that the 2d input conversion board method will convert the position to the board
 def test_BitBoard_0_previous_positions():
     bitboard = BitBoard(num_previous_positions=0)
-
     input_board = chess.Board()
     model_input = bitboard.board_to_model_input(input_board)
     reconverted_board = bitboard.model_input_to_board(model_input)
     
 
     model_input_shape = np.shape(model_input)
-    expected_shape = np.array((12, 8, 8))
+    expected_shape = np.array((1, 12, 8, 8))
     assert np.all(model_input_shape == expected_shape) 
+
+    print(reconverted_board)
+    print("===============")
+    print(input_board)
+    print("===============")
+    print(model_input)
+
     assert reconverted_board == input_board  
 
-# Tests that the 2d input conversion board method will convert the position in cases where depth is kept
+
 def test_PieceMap_0_previous_positions():
     piecemap = PieceMap(num_previous_positions=0)
-
     input_board = chess.Board()
     model_input = piecemap.board_to_model_input(input_board)
     reconverted_board = piecemap.model_input_to_board(model_input)
@@ -46,19 +50,24 @@ def test_PieceMap_0_previous_positions():
     assert np.all(model_input_shape == expected_shape)
     assert reconverted_board == input_board
 
-    """PLY = 5
-    board = chess.Board()
+
+def test_BitBoard_5_previous_positions():
+    input_board = chess.Board()
     moves = ['e4', 'e5', 'Qh5', 'Nc6', 'Bc4', 'Nf6', 'Qxf7']
     for moven in moves:
-        board.push_san(moven)
+        input_board.push_san(moven)
+    
+    PLY = 5
+    bitboard = BitBoard(num_previous_positions=PLY)
+    model_input = bitboard.board_to_model_input(input_board)
+    reconverted_board = bitboard.model_input_to_board(model_input)
 
-    input_board = board_to_input_2d(board, PLY)
-    reconverted_board = input_to_board_2d(input_board, PLY)
+    model_input_shape = np.shape(model_input)
+    expected_shape = np.array((PLY+1, 12, 8, 8))
 
-    assert reconverted_board.ply() == PLY
-    assert board.ply() >= PLY
-    assert reconverted_board == board
-    """
+    assert np.all(model_input_shape == expected_shape) 
+    assert input_board.ply() >= PLY
+    assert reconverted_board == input_board
     
 
 """def test_move_to_flat():
